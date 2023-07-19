b251738251f17ac868d31e9e1487c3567ca73974
joseluistello <jltello96@gmail.com>
2023-07-19 01:59:01 -0600

    adds

--- components/ui/headersection.tsx
+++ components/ui/headersection.tsx
@@ -19,7 +19,7 @@ return (
           <a className="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/login"></a>
         </div>
         <a className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600" href="/register">
-          <span>Login<span className="hidden lg:inline"></span></span>
+          <span>Sign in<span className="hidden lg:inline"></span></span>
         </a>
         <div className="-mr-1 md:hidden">
           <div data-headlessui-state="">
